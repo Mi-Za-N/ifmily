7f0203228acddc8e39ef0ace6ecd77d332377fa3
Mizan <mizan.softdev@gmail.com>
2021-02-12 22:30:46 +0600

    update mobile cartbutton

--- src/components/cart-popup/cart-popup-button.js
+++ src/components/cart-popup/cart-popup-button.js
@@ -29,8 +29,7 @@ const CartPopupButton = ({
       {itemCount} {itemPostfix}
     </ItemCount>
     <PriceBox>
-      {/* {pricePrefix}
-      {price} */}
+      {pricePrefix}
       {parseFloat(`${price}`).toFixed(2)}
     </PriceBox>
   </CartPopupButtonStyled>
